16fc5a984a2021aea1e427d760494ac071f8c805
ejfunk <emersyn_funk@taylor.edu>
2023-11-01 19:08:08 -0400

    updated

--- db/migrate/20231028204039_combine_items_in_cart.rb
+++ db/migrate/20231028204039_combine_items_in_cart.rb
@@ -23,13 +23,13 @@ class CombineItemsInCart < ActiveRecord::Migration[7.0]
       # add individual items
       line_item.quantity.times do
         LineItem.create(
-        cart_id: line_item.cart_id,
-        product_id: line_item.product_id,
-        quantity: 1
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
         )
         end
         # remove original item
         line_item.destroy
         end
-        end
+      end
 end
