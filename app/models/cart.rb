5e5716ce3414213bec555d6b8e02247cadff473a
ejfunk <emersyn_funk@taylor.edu>
2023-11-01 19:08:10 -0400

    message

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
-class Cart < ApplicationRecord\
-    has_many:line_item, dependent: :destroy
+class Cart < ApplicationRecord
+    has_many :line_items, dependent: :destroy
     def total_price
         line_items.sum { |item| item.total_price }
         end
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -48,4 +48,13 @@ assert_select 'h2', 'Your Pragmatic Cart'
 
     assert_redirected_to line_items_url
   end
+
+  test "can't delete product in cart" do
+     assert_difference("Product.count", 0) do
+     delete product_url(products(:two))
+     end
+    
+     assert_redirected_to products_url
+     end
+    
 end
