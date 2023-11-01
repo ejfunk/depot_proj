78b56e3399971df4ec220ae5965dae95450c7d66
ejfunk <emersyn_funk@taylor.edu>
2023-11-01 19:26:03 -0400

    part 11 part 1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,12 @@ before_action :set_cart, only: %i[create]
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
+        format.turbo_stream 
+           partial: 'layouts/cart',
+           locals: { cart: @cart }
+           
+           end
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
