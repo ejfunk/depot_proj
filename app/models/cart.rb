7179ba2daa9c10040869ac5dbc49743d2cb99b03
ejfunk <emersyn_funk@taylor.edu>
2023-12-06 09:32:51 -0500

    internationalization

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,5 +11,7 @@ class Cart < ApplicationRecord
         current_item = line_items.build(product_id: product.id)
         end
         current_item
-        end        
+        end   
+
+     
 end
